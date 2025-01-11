78fbd17574cecd0f0e8a266cb3f1955ce6538fef
bokiko <124489102+bokiko@users.noreply.github.com>
2025-01-11 12:29:50 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
-import CryptoTable from '../components/CryptoTable'
-import { Suspense } from 'react'
+'use client';
+
+import CryptoTable from '../components/CryptoTable';
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-4 bg-white">
-      <Suspense fallback={<div>Loading...</div>}>
-        <CryptoTable />
-      </Suspense>
+    <main>
+      <CryptoTable />
     </main>
-  )
+  );
 }
