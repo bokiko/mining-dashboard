13f1e563a0b1c0bb82c7713e1c986f7af1e8d669
bokiko <124489102+bokiko@users.noreply.github.com>
2025-01-11 21:00:08 +0300

    Update tailwind.config.js

--- src/app/tailwind.config.js
+++ src/app/tailwind.config.js
@@ -2,7 +2,27 @@ module.exports = {
   purge: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3490dc',
+        secondary: '#ffed4a',
+        tertiary: '#6574cd',
+      },
+      animation: {
+        'fade-in': 'fadeIn 1s ease-out',
+        'slide-in': 'slideUp 1s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        slideUp: {
+          from: { transform: 'translateY(100px)' },
+          to: { transform: 'translateY(0)' },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
